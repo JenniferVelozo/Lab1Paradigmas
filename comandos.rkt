12d666094ac27364c4e1262782d5ff3ac95f6215
Jennifer Velozo <jennifer.velozo@usach.cl>
2020-05-23 20:59:31 -0400

    funcion commit y algunas modificaciones

--- comandos.rkt
+++ comandos.rkt
@@ -1,22 +1,45 @@
 #lang racket
 (require "TDAzonas.rkt")
+(require "TDAcommit.rkt")
+(require "funcionesGenerales.rkt")
 
 (define workspace (list (list "archivo1" "contenido1") (list "archivo2" "contenido2") (list "archivo3" "contenido3")))
-(define index '())
+(define index (list (list "uno.c" "codigo1") (list "dos.c" "codigo2")))
 (define localR '())
 (define remoteR '())
-
 (define zonas (zonasCons workspace index localR remoteR))
 
+;desc: función que añade los archivos de la lista 1, contenidos en el workspace, a una lista 2
+;dom: lista x lista x lista
+;rec: lista 
 (define myAdd (lambda (workspace L1 L2)
                                     (if (null? L1)
                                         L2
                                         (if (null? workspace)
                                             null
                                             (if (equal? (car L1) (caar workspace))
-                                                (myAdd workspace(cdr L1)(append L2 (list(car workspace))))
+                                                (myAdd workspace(cdr L1)(myAppend (car workspace) L2))
                                                 (myAdd (cdr workspace) L1 L2))))))
 
+;desc:Función que añade los cambios locales registrados en el Workspace al Index registrados en las zonas de trabajo
+;dom: lista 
+;rec: lista (nueva versión de zonas donde se ven reflejados los cambios)
 (define add (lambda (lista) (lambda (zonas)
-                              (setIndex zonas (myAdd (zonaWorkspace zonas) lista '() )))))
+                              (setIndex zonas (myAdd (zonaWorkspace zonas) lista '()) ))))
 
+;desc: función que crea un commit si hay cambios en el Index
+;dom: string x lista
+;rec: commit o 0 en caso que no hayan cambios en el Index
+(define commitAux (lambda(mensaje zonas)
+                    (if (null? (zonaIndex zonas))
+                        0
+                        (commitCons mensaje (zonaIndex zonas)))))
+                    
+;desc: Función que genera un commit con los cambios almacenados en index especificando un mensaje descriptivo (un string) para llevarlos al LocalRepository 
+;dom: string
+;rec: lista (nueva versión de zonas donde se ven reflejados los cambios.
+(define commit (lambda (mensaje)
+                 (lambda (zonas)
+                   (if (equal? (commitAux mensaje zonas) 0)
+                       (display "No hay cambios en el Index")
+                       (setIndex (setLocalR zonas (myAppend (commitAux mensaje zonas) (zonaLocalR zonas))) '()))))) ;el Index queda vacío
---
+++ funcionesGenerales.rkt
@@ -0,0 +1,50 @@
+#lang racket
+
+;desc:  entrega último elemento de la lista
+;dom: Lista
+;rec: Elemento de Lista
+;recursión: de cola
+(define getLast ( lambda (L) (if (null? (cdr L));si no queda nada en L
+                                 (car L);devolver la cabeza => (algo, null)
+                                 (getLast (cdr L))
+                                 )))
+
+
+;Desc: agrega un elemento al final de una lista
+;Dominio: elemento x lista
+;Recorrido: una lista con el elemento agregado
+;Recursión: natural
+(define myAppend (lambda(elemento lista)
+                 (if (null? lista)
+                     (cons elemento null)
+                     (cons (car lista) (myAppend elemento (cdr lista)))
+                  )))
+
+;Función que une dos listas
+;Dominio: lista x lista
+;Recorrido: una lista
+;Recursión: narural
+(define myAppend2 (lambda (L1 L2)
+                     (if (null? L1)
+                         L2
+                         (if (null? L2)
+                             L1
+                             (cons (car L1) (myAppend2 (cdr L1) L2))))))
+
+;Permite obtener el elemento ubicado en la posición que se indique
+;Dominio: posicion x lista
+;Recorrido: entrega el elemento que se encuentra en la posición indicada
+;Recursión: natural
+(define obtenerElemento (lambda (pos lista)
+  (if (list? lista)
+      (if (null? lista)
+          lista
+          (if (= pos 0)
+              (car lista)
+              (obtenerElemento (- pos 1)(cdr lista))
+              )
+          )
+      null)
+  ))
+
+(provide (all-defined-out))
